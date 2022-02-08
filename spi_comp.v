3d771f6666042bcb9dfb2a5cbce57f5966ff2633
Abhishek7799-max <70936090+Abhishek7799-max@users.noreply.github.com>
2022-02-08 11:02:45 +0530

    Add files via upload
    This is an implementation of an SPI(Serial Peripheral Interface) protocol using the Verilog HDL. SPI is a full-duplex, faster than UART, I2C communication protocols. The design is done for one master and two slaves. The use of 8-bit registers is done to understand the operation and store the data. The dtf signal[data transfer] works with four different modes and the explanation of it can be given as below. 
    
    dtf = 2'b00 means no communication 
             2'b01 means data from slave to master
             2'b10 means data from master to slave
             2'b11 means full duplex communication

---
+++ spi_comp.v
@@ -0,0 +1,174 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:34:53 02/07/2022 
+// Design Name: 
+// Module Name:    spi_mak 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+// full duplex, 
+// faster than UART, I2C 
+// taking dtf as data transfer signal
+// half duplex communication
+/* 
+dtf can be 2 bit signal
+dtf = 2'b00 means no communication 
+      2'b01 means data from slave to master
+		2'b10 means data from master to slave
+		2'b11 means full duplex communication		
+*/
+
+module spi_masterf( input clk, rst, miso, // flag = 0 means data from slave to master and vice versa 
+input [1:0] slave, dtf, 
+output reg sclk, mosi, ssb1, ssb2
+    );
+	 
+reg [7:0] stor_reg1,stor_reg2;
+reg [2:0] k,p;
+always @(clk)
+begin
+if(rst)
+begin
+stor_reg1 = 8'b01110101;
+//stor_reg2 = 8'b01010010;
+k = 0;
+p = 0;
+sclk = 0;
+mosi = 0;
+ssb1 = 0; // ssb is active low version of ss, ss = !ssb 
+ssb2 = 0;
+end
+
+else if(dtf == 2'b01 || dtf == 2'b10 || dtf == 2'b11)
+begin
+sclk = clk;
+
+if(clk)
+begin
+if(slave == 2'b01)
+begin
+ssb1 = 1;
+ssb2 = 0;
+end
+else if(slave == 2'b10)
+begin
+ssb2 = 1;
+ssb1 = 0;
+end
+else
+begin
+ssb1 = 0;
+ssb2 = 0;
+end
+end
+
+if(dtf == 2'b10 & clk)
+begin
+mosi = stor_reg1[k];
+k = k + 1;
+end
+else if (dtf == 2'b01 & clk) 
+begin
+stor_reg2[p] = miso;
+p = p + 1;
+end
+else if (dtf == 2'b11 & clk)
+begin
+mosi = stor_reg1[k];
+k = k + 1;
+stor_reg2[p] = miso;
+p = p + 1;
+end
+end
+
+end
+
+endmodule
+
+module spi_slave1f(input sclk, mosi, ssb, 
+input [1:0] dtf, // flag = 0 means data from slave to master and vice versa
+output reg miso
+);
+reg [7:0] stor_reg1,stor_reg2;
+reg [2:0] i,j;
+
+initial 
+begin
+stor_reg2 = 8'b01010010;
+i = 0;
+j = 0;
+end 
+
+always @(posedge sclk)
+begin
+if(((dtf == 2'b10) || (dtf == 2'b11) )&& ssb)
+begin
+stor_reg1[i] = mosi;
+i = i + 1;
+end
+if(((dtf == 2'b01) || (dtf == 2'b11) )&& ssb)
+begin
+miso = stor_reg2[j];
+j = j + 1;
+end
+
+end
+endmodule
+
+module spi_slave2f(input sclk, mosi, ssb, 
+input [1:0] dtf, // flag = 0 means data from slave to master and vice versa
+output reg miso
+);
+reg [7:0] stor_reg1,stor_reg2;
+reg [2:0] i,j;
+
+initial 
+begin
+stor_reg2 = 8'b01010010;
+i = 0;
+j = 0;
+end 
+
+always @(posedge sclk)
+begin
+if(((dtf == 2'b10) || (dtf == 2'b11) )&& ssb)
+begin
+stor_reg1[i] = mosi;
+i = i + 1;
+end
+if(((dtf == 2'b01) || (dtf == 2'b11) )&& ssb)
+begin
+miso = stor_reg2[j];
+j = j + 1;
+end
+
+end
+endmodule
+
+
+
+module spi_topf (clk, rst, miso, mosi, dtf, sclk, ssb1, ssb2, slave );
+input clk, rst;
+inout miso, mosi;
+inout sclk, ssb1, ssb2;
+input [1:0] slave, dtf;
+wire miso1, miso2;
+assign miso = ((slave == 2'b01) ? miso1 : miso2) && ((dtf == 2'b01) || (dtf == 2'b11));
+spi_masterf p1 (clk, rst, miso, slave, dtf, sclk, mosi, ssb1, ssb2 );
+spi_slave1f p2 (sclk, mosi, ssb1, dtf, miso1);
+spi_slave2f p3 (sclk, mosi, ssb2, dtf, miso2);
+
+endmodule
---
+++ spi_tbf.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:25:09 02/08/2022
+// Design Name:   spi_topf
+// Module Name:   E:/DA study/Practice/Practice_1/spi_tbf.v
+// Project Name:  Practice_1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: spi_topf
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module spi_tbf;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg [1:0] dtf;
+	reg [1:0] slave;
+
+	// Bidirs
+	wire miso;
+	wire mosi;
+	wire sclk;
+	wire ssb1;
+	wire ssb2;
+
+	// Instantiate the Unit Under Test (UUT)
+	spi_topf uut (
+		.clk(clk), 
+		.rst(rst), 
+		.miso(miso), 
+		.mosi(mosi), 
+		.dtf(dtf), 
+		.sclk(sclk), 
+		.ssb1(ssb1), 
+		.ssb2(ssb2), 
+		.slave(slave)
+	);
+
+	
+		initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 1;
+		//miso = 0;
+		dtf = 0;
+		//flag = 0;
+		slave = 0;
+
+		// Wait 100 ns for global reset to finish
+		#10;
+		rst = 0;
+		dtf = 2'b10;
+		//miso = 0;
+		//flag = 1;
+		slave = 2'b01;
+        #6;
+		  slave = 2'b10;
+		  #10;
+		  dtf = 2'b11;
+		  //flag = 0;
+		 #14;
+		 dtf = 0;
+		// Add stimulus here
+
+	end
+      
+		always #1 clk = !clk;
+      
+endmodule
+
